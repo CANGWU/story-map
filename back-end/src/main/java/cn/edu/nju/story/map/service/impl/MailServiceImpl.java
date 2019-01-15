2e0460b61d306ff46926f6e957ea4788e14773fd
xuan <1533704796@qq.com>
2019-01-15 11:48:41 +0800

    tmp

--- back-end/src/main/java/cn/edu/nju/story/map/service/impl/MailServiceImpl.java
+++ back-end/src/main/java/cn/edu/nju/story/map/service/impl/MailServiceImpl.java
@@ -1,7 +1,14 @@
 package cn.edu.nju.story.map.service.impl;
 
 import cn.edu.nju.story.map.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * @author xuan
@@ -11,11 +18,30 @@ import org.springframework.stereotype.Service;
 public class MailServiceImpl implements MailService {
 
 
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
     @Override
     public void sendInvitation(Long userId, String username, String email) {
 
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        String from = ((JavaMailSenderImpl) javaMailSender).getJavaMailProperties().getProperty("from");
+        String subject = ((JavaMailSenderImpl) javaMailSender).getJavaMailProperties().getProperty("subject");
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(from);
+            helper.setTo(email);
+            helper.setSubject(subject);
 
-
+            StringBuffer sb = new StringBuffer();
+            sb.append("<h1>大标题-h1</h1>")
+                    .append("<p style='color:#F00'>红色字</p>")
+                    .append("<p style='text-align:right'>右对齐</p>");
+            helper.setText(sb.toString(), true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException ignore) {
+        }
 
     }
 }
